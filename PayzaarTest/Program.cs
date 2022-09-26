b20a61df5093cb78007bd75616711f5414550029
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2022-09-26 22:43:57 +0100

    Adding instruction comments

--- PayzaarTest/Program.cs
+++ PayzaarTest/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+/*
+* TODO:
+*	- refactor, find all the issues you can and fix them
+*	- change the code to follow clean architecture principles
+*	- apply design patterns where you think it is required
+*	- do not change the output
+*	- make sure the refactored code is testable (you are allowed to write a test or two as a PoC)
+*	- add comments, highlighting what you changed and what was the reason for change
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
